fe4fd75f9fd1b544be332453d125ab2f631ae194
bbangwoo <155467896+bbangwoo@users.noreply.github.com>
2024-01-11 02:09:50 +0900

    Update telegramAPI.py

--- telegramAPI.py
+++ telegramAPI.py
@@ -9,11 +9,11 @@ async def send_daily_message(): #실행시킬 함수명 임의지정
     chat_id = 5959976794
     bot = telegram.Bot(token = token)
 
-    message = "오늘도 힘내보자"
+    message = "오늘도 힘내보자 빠이팅~"
     await bot.send_message(chat_id,message)
 
 
-target_time = "2024-01-11 02:00:10"
+target_time = "2024-01-11 09:00:10"
 clock = datetime.datetime.strptime(target_time, "%Y-%m-%d %H:%M:%S")
 import time
 
